5dc83deb06b98c196ac1669a5ed44a279af6667e
Shivani-kumari <49039537+Shivani-kumari@users.noreply.github.com>
2019-11-02 12:34:10 +0530

    Variable Sized Array

--- Untit.cpp
+++ Untit.cpp
@@ -7,16 +7,24 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    int  n ; 
-    cin >> n ;
-    int arr[n];
-    for(int i = 0; i<n;i++){
-        cin>>arr[i] ;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n,q,l,k,m;
+    cout<<"number of array and nuber of query";
+    cin >> n >> q  ; 
+    for(int i=0;i<n ; i++){
+        int l ;
+        cout<<"Enter the length of array";
+        cin>> l;
+        int arr[l];
+		cout<<"Enter array value";
+        for(int j=0;j<l;j++)
+        cin>>arr[j];
+      k++;
+        int index;
+        cout<<"  Enter your qrery \n";
+        cin>> index;
+        cout<<arr[index];
     }
-     for(int k=n-1;k>=0; k--){
-         cout<<arr[k] <<" " ;
-     }
-
+    
     return 0;
 }
